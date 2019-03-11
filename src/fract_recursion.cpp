542132d274f54a988b5b03d9a6d7fc93a250f653
Apurba <apurba10024@gmail.com>
2019-03-11 23:59:32 +0600

    1.3 Added contorSet

--- src/fract_recursion.cpp
+++ src/fract_recursion.cpp
@@ -1,11 +1,3 @@
-/*
- * CS 106B/X Sample Project
- * last updated: 2018/09/19 by Marty Stepp
- *
- * This project helps test that your Qt Creator system is installed correctly.
- * Compile and run this program to see a console and a graphical window.
- * If you see these windows, your Qt Creator is installed correctly.
- */
 
 #include <iostream>
 #include "console.h"
@@ -23,7 +15,7 @@ int main() {
     window.setWindowTitle("CS 106B/ x fractals");
     window.setColor("black");
 
-    cantorSet(window, 50, 50, 700, 1);
+    cantorSet(window, 50, 50, 700, 8);
     return 0;
 }
 
@@ -34,10 +26,15 @@ int main() {
  *  ---    ---            ---    ---
  *  - -    - -            - -    - -
  */
-
 void cantorSet(GWindow& window, int x, int y, int length, int levels){
-    if(levels == 1){
-        // base case
-        window.drawLine(x,y, x+length, y);
+    if(levels > 0){
+        window.drawLine(x, y, x+length, y); // big level line
+        window.drawLine(x, y+1, x+length, y+1); // big level line
+        window.drawLine(x, y+2, x+length, y+2); // big level line
+
+        pause(60);
+
+        cantorSet(window, x,            y + 20, length/3, levels-1); // left line
+        cantorSet(window, x+2*length/3, y + 20, length/3, levels-1); // right line
     }
 }
